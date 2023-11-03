3cdd0f0db0ee592081fc75fc0152dc16f7ae6a4c
swaraj Saxena <swarishraj00@gmail.com>
2023-11-03 23:06:57 +0530

    auth bug fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,24 +1,5 @@
-import GoogleProvider from 'next-auth/providers/google'
-import GithubProvider from 'next-auth/providers/github'
+import { authOptions } from '@/utils/authOptions'
 import NextAuth from 'next-auth'
-
-export const authOptions = {
-  secret: 'ink_it_swaraj_saxena_240900',
-  providers: [
-    GoogleProvider({
-      clientId:
-        '402397592714-bn0sirekfmfci1gmdhet32q804mh6uma.apps.googleusercontent.com',
-      clientSecret: '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',
-    }),
-    GithubProvider({
-      clientId: 'Iv1.884af479c8216d98',
-      clientSecret: '20a56856c7f5dc47c640a82c279868f981a411e8',
-    }),
-  ],
-  pages: {
-    signIn: '/auth/signin',
-  },
-}
 
 const handler = NextAuth(authOptions)
 export { handler as GET, handler as POST }
